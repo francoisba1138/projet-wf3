46fbcc717dd0b69102b69e2c95752a3fffd7b7a5
Louis Grandclerc <louis.grandclerc@outlook.com>
2019-04-11 16:53:59 +0200

    API Controller

---
+++ src/Controller/APIController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Buzz\Browser;
+use Buzz\Client\BuzzClientInterface;
+use Buzz\Client\FileGetContents;
+use GuzzleHttp\Client;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class APIController extends AbstractController
+{
+    /**
+     * @Route("/api/search/{name}")
+     */
+    public function index($name)
+    {
+
+        $client = new FileGetContents(new Psr17Factory());
+        $browser = new Browser($client, new Psr17Factory());
+
+        $response = $browser->get(
+            "https://api-v3.igdb.com/games?fields=*&search=" . $name,
+
+                [
+                    'user-key' => '128f06a547525a39878205e49b57fa50',
+                    "Accept" => "application/json"
+                ]
+        );
+
+        return $this->render(
+            'api/index.html.twig', [
+            'response' => json_decode($response->getBody()->getContents())
+        ]);
+    }
+}
