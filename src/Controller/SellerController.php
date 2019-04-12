ccd627bbd4511a3f7bbb95d8acd359b0e0f4df09
francoisba1138 <francoisba@gmail.com>
2019-04-12 16:57:56 +0200

    profils index buyers sellers

---
+++ src/Controller/SellerController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Class SellerController
+ * @package App\Controller
+ *
+ * @Route("/vendeur")
+ */
+class SellerController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+
+        $repository = $this->getDoctrine()->getRepository(User::class);
+
+        $sellers =$repository->findBy(
+            [
+                'role' => 'ROLE_SELLER'
+            ]
+
+        );
+
+
+        return $this->render('seller/index.html.twig',
+        [
+            'sellers' => $sellers
+
+        ]
+            );
+
+
+    }
+
+    /**
+     * @Route("/{id}", requirements={"id": "\d+"})
+     */
+    public function detail(User $seller)
+
+
+    {
+        dump($seller);
+
+        $role= $seller->getRole();
+
+        if ($role=='ROLE_SELLER') {
+
+
+            return $this->render('seller/profile.html.twig',
+                [
+                    'seller' => $seller
+                ]
+            );
+
+        } else {
+
+            return $this->redirectToRoute('app_index_index');
+        }
+
+    }
+}
