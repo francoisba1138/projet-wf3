b4e127255fceba53c4caa164b855e6de4f94260c
guillaume77910 <guillaume.gosset0405@orange.fr>
2019-04-15 17:23:14 +0200

    commit endday

--- src/Controller/Admin/BuyerController.php
+++ src/Controller/Admin/BuyerController.php
@@ -85,7 +85,7 @@ class BuyerController extends AbstractController
     public function ajaxContent(Request $request, User $buyer)
     {
 
-        if( $request->isXmlHttpRequest() ) {
+        if( $request->isXmlHttpRequest()) {
 
 
 
