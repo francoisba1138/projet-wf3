f0d647b150f54675b9d690c8d84d97d626ef2f5a
francoisba1138 <francoisba@gmail.com>
2019-04-23 16:32:59 +0200

    admin

--- src/Controller/Admin/CommentController.php
+++ src/Controller/Admin/CommentController.php
@@ -3,7 +3,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Comment;
+
+use App\Form\CommentadminType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -21,8 +25,18 @@ class CommentController extends AbstractController
     public function index()
     {
 
-        return $this->render('admin/comment/index.html.twig');
+        $repository = $this->getDoctrine()->getRepository(Comment::class);
 
+
+        $comments = $repository->findAll();
+
+        return $this->render('admin/comment/index.html.twig',
+
+            [
+                'comments' => $comments
+
+            ]
+        );
 
     }
 
@@ -30,16 +44,78 @@ class CommentController extends AbstractController
     /**
      * @Route("/{id}")
      */
-    public function detail()
+    public function detail(Comment $comment, Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(CommentadminType::class, $comment);
+        $form->handleRequest($request);
 
-        return $this->render('admin/comment/detail.html.twig');
+        if( $form->isSubmitted() ) {
+            if( $form->isValid() ) {
+
+
+                $em->persist($comment);
+                $em->flush();
+                $this->addFlash('success', "Le commentaire est enregistré");
+                return $this->redirectToRoute('app_admin_comment_index');
+
+            } else {
+                $this->addFlash('error', 'Le formulaire contient des erreurs');
+
+            }
+        }
+
+
+        return $this->render('admin/comment/detail.html.twig',
+
+
+            [
+                'form' => $form->createView(),
+                'comment' => $comment
+
+
+            ]
+
+        );
 
 
     }
 
+    /**
+     * @Route("/suppression/{id}")
+     */
+    public function delete(Comment $comment)
+    {
+        $em = $this->getDoctrine()->getManager();
 
 
+        $em->remove($comment);
+        $em->flush();
+        $this->addFlash('success', "Le commentaire est supprimé");
+        return $this->redirectToRoute('app_admin_comment_index');
 
 
+    }
+
+    /**
+     * @Route("/ajax/content/{id}")
+     */
+    public function ajaxContent(Request $request, Comment $comment)
+    {
+
+        if( $request->isXmlHttpRequest() ) {
+
+            return $this->render(
+                'admin/comment/ajax_content.html.twig',
+                ['comment' => $comment]
+            );
+
+
+        } else {
+
+            throw new NotFoundHttpException();
+
+        }
+
+    }
 }
--- src/Form/AdadminType.php
+++ src/Form/AdadminType.php
@@ -7,14 +7,14 @@ use App\Entity\Game;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AdType extends AbstractType
+class AdadminType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
